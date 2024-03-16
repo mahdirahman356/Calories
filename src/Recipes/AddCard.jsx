5f1511f67e35627c5cdb626528eb99a03fa7033e
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-03-16 21:38:25 +0600

    setup Currently cooking

--- src/Recipes/AddCard.jsx
+++ src/Recipes/AddCard.jsx
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types'; 
 import AddCurrently from './AddCurrently';
+import { useState } from 'react';
 const AddCard = ({adding}) => {
-    
-    
+
+  let [preparing,setPreparing] = useState([])
+  let hendlePreparing = (pre) => {
+    let AllPreparing = [...preparing,pre]
+    setPreparing(AllPreparing)
+}
     return (
         <div className="md:w-[40%] border-[1px] border-[#28282833] rounded-xl">
             <h2 className="text-2xl font-bold text-center my-5 border-b-[1px] border-[#2828281A] pb-4 w-[80%] mx-auto">Want to cook: {adding.length}</h2>
@@ -21,11 +26,12 @@ const AddCard = ({adding}) => {
               <td>{add.name}</td>
               <td>{add.preparing_time}</td>
               <td>{add.calories}</td>
-              <td><button className="btn border-none bg-[#0BE58A] rounded-[30px]">Preparing</button></td>
+              <td><button onClick={()=>hendlePreparing(add,add.id)} className="btn border-none bg-[#0BE58A] rounded-[30px]">Preparing</button></td>
             </tr>
           )}
           </table>  
-           <AddCurrently></AddCurrently>
+          <AddCurrently preparing ={preparing}></AddCurrently>
+           
         </div>
     );
 };
--- src/Recipes/AddCurrently.jsx
+++ src/Recipes/AddCurrently.jsx
@@ -1,16 +1,37 @@
 
+import PropTypes from 'prop-types'; 
 
-const AddCurrently = () => {
+const AddCurrently = ({preparing}) => {
+    console.log(preparing)
     return (
-        <div>
-            <h2 className="text-2xl font-bold text-center my-5 border-b-[1px] border-[#2828281A] pb-4 w-[80%] mx-auto">Currently cooking:</h2>
-            <div className="w-[80%] mx-auto flex justify-between text-[#878787] font-semibold">
-              <p>Name</p>
-              <p>Time</p>
-              <p>Calories</p>
-            </div>
+        <div> 
+
+<h2 className="text-2xl font-bold text-center my-5 border-b-[1px] border-[#2828281A] pb-4 w-[80%] mx-auto">Currently cooking: {preparing.length}</h2>
+            <table className="w-[100%]">
+            <tr className="text-center"> 
+              <td></td>
+              <td>Name</td>
+              <td>Time</td>
+              <td>Calories</td> 
+            </tr> 
+            
+            {preparing.map((pre,index) =>      
+            <tr key={index} className="text-center bg-[#28282808]">  
+             <td className="w-5"><div className="px-3">{index + 1}</div></td>
+              <td>{pre.name}</td>
+              <td>{pre.preparing_time}</td>
+              <td>{pre.calories}</td>
+              </tr>   
+          )}
+            </table>
+
+
+           
         </div>
     );
 };
 
+AddCurrently.propTypes={
+  preparing:PropTypes.array.isRequired
+}
 export default AddCurrently;
